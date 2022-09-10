48e60fc079560ee0bb6ce4c06720e4c012ee924a
DONHACKKURD <110987509+DONHACKKURD@users.noreply.github.com>
2022-09-10 20:55:44 +0300

    Add files via upload

---
+++ rodehack.sh
@@ -0,0 +1,277 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+echo
+python2 meizu.py
+echo
+clear
+echo
+echo $red"                DON HACK VIP TOOLS "
+echo $red"            OWNER : V_86Q RODE KURD("   
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo
+echo
+echo
+echo
+echo $red"                DON HACK VIP TOOLS "
+echo $red"            OWNER : V_86Q RODE KURD"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo
+echo
+echo $red"                DON HACK VIP TOOLS "
+echo $red"            OWNER : V_86Q RODE KURD"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"               DON HACK VIP TOOLS "
+echo $red"            OWNER : V_86Q RODE KURD"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"                DON HACK VIP TOOLS "
+echo $red"            OWNER : V_86Q RODE KURD"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+echo
+echo $pur"==================="$cy" ============================="
+echo $i"Ôûê"$pu"  AUTHOR    :  DonHack  "$i"               Ôûê"
+echo $i"Ôûê"$pu"  TIKTOK    :  1_donhack "$i"                Ôûê"
+echo $i"Ôûê"$pu"  GMAIL     :  donhack650@gmail.com"$i" Ôûê"
+echo $i"Ôûê"$pu"  YOUTUBE   :  DON HACK"$i"               Ôûê"
+echo $i"Ôûê"$pu"  INSTAGRAM :  V_86Q1"$i"                 Ôûê"
+echo $i"Ôûê"$pu"  TELEGRAM  :  V_86Q "$i"                 Ôûê"
+echo $pur"==================="$cy" ============================="
+echo
+echo $cy"Download Dunhak Tool:"
+echo $ku"["$me"1"$ku"]"$i" REPORT FB"
+echo $ku"["$me"2"$ku"]"$i" BRUTE PORCE"
+echo $ku"["$me"3"$ku"]"$i" BRUTE FORCE FB CRACKER"
+echo $ku"["$me"4"$ku"]"$i" MBF"
+echo $ku"["$me"5"$ku"]"$i" CLONING YAHOO"
+echo $ku"["$me"6"$ku"]"$i" OSIF"
+echo $ku"["$me"7"$ku"]"$i" PROFILEGUARD FB"
+echo $ku"["$me"8"$ku"]"$i" BOT REACTION FACEBOOK"
+echo $ku"["$me"9"$ku"]"$i" SPAM CALL"
+echo $ku"["$me"10"$ku"]"$i" SPAM WHATSHAPP"
+echo $ku"["$me"11"$ku"]"$i" YOUTUBE-DL"
+echo $ku"["$me"12"$ku"]"$i" LACAK LOKASI"
+echo $ku"["$me"13"$ku"]"$i" SPAM SMS BRUTAL TERBARU"
+echo $i"["$me"14"$i"]"$ku" EXIT"
+echo
+echo $pur"Ôò¡ÔöÇ["$pur"wait..."$pur"]"
+read -p"Ôò¦ÔöÇ>> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
+cd multi-bruteforce-facebook
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/jaxBCD/FBshield.git
+cd FBshield
+python2 guard.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/edi-nasa12/ROBOT.git
+cd ROBOT
+python2 blackbot.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/404rgr/spamerCALL
+cd spamerCALL
+python spamercall.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/rahmadxyz/sub-bot.git
+cd sub-bot
+php sub-bot.php
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/thewhiteh4t/seeker
+cd seeker.py
+python seeker.py -t manual
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "D O N I"|lolcat
+sleep 1
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "D O N I"|Ulolcat
+sleep 2
+echo $cy"DON HACK Tools"
+sleep 2
+echo $i"OWNER : V_86Q"
+sleep 2
+echo $pur"DON HACK VIP KURD"
+sleep 2
+echo $ku"TikTok   :"$i" 1_donhack  "
+echo $ku"YouTube  :"$i" DON HACK"
+echo $ku"Telegram :"$i" @V_86Q  "
+sleep 2
+echo $pur" DON HACK VIP TOOLS"
+sleep 2
+echo $pur" MY CHANNEL { @V_86Q1}"
+exit
+fi
